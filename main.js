ffb2fa5b39d59e9d854efe14345fbe52eb24f704
higenesio <129765625+higenesio@users.noreply.github.com>
2023-11-27 11:01:17 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,5 +2,37 @@
 function TocarSomAplausos(){
 
 document.querySelector('#som_tecla_aplausos').play();}
-
 document.querySelector('.tecla_aplausos').onclick=TocarSomAplausos;
+
+function TocarSomVaia(){
+document.querySelector('#som_tecla_vaia').play();}
+document.querySelector('.tecla_vaia').onclick=TocarSomVaia;
+
+function TocarSomRisada(){
+document.querySelector('#som_tecla_risada').play();}
+document.querySelector('.tecla_risada').onclick=TocarSomRisada;
+
+function TocarSomErrado(){
+document.querySelector('#som_tecla_errado').play();}
+document.querySelector('.tecla_errado').onclick=TocarSomErrado;
+
+function TocarSomGrilo(){
+document.querySelector('#som_tecla_grilo').play();}
+document.querySelector('.tecla_grilo').onclick=TocarSomGrilo;
+
+function TocarSomTambores(){
+document.querySelector('#som_tecla_tambores').play();}
+document.querySelector('.tecla_tambores').onclick=TocarSomTambores;
+
+function TocarSomTrombeta(){
+document.querySelector('#som_tecla_trombeta').play();}
+document.querySelector('.tecla_trombeta').onclick=TocarSomTrombeta;
+
+function TocarSomDinheiro(){
+document.querySelector('#som_tecla_dinheiro').play();}
+document.querySelector('.tecla_dinheiro').onclick=TocarSomDinheiro;
+
+function TocarSomVitória(){
+document.querySelector('#som_tecla_vitoria').play();}
+document.querySelector('.tecla_vitoria').onclick=TocarSomVitória;
+
